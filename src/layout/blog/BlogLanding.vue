536401dfc178da0abf12e1391935d27e1c344728
Aldon <aisenberg@freightview.com>
2017-12-04 22:53:07 -0600

    push

--- src/layout/blog/BlogLanding.vue
+++ src/layout/blog/BlogLanding.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
     <Blog-Home></Blog-Home>
+    <Blog-Navigation></Blog-Navigation>
   </div>
 </template>
 
 <script>
 import BlogHome from './BlogHome.vue'
+import BlogNavigation from './BlogNavigation.ve'
 export default {
   components: {
-    'Blog-Home': BlogHome
+    'Blog-Home': BlogHome,
+    'blog-Navigation': BlogNavigation
   }
 }
 </script>
