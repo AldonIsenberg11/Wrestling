886e9cab75d6e4c6314035dbe78028994c98bd3c
Aldon <aisenberg@freightview.com>
2017-12-06 18:07:03 -0600

    quick fix

--- src/layout/AppLogin.vue
+++ src/layout/AppLogin.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="container">
-    <div class="navbar is-fixed-top">
-      <div class="columns">
-        <div class="column">
-          <input class="input" type="text" placeholder="Your username">
-          <input class="input" type="password" placeholder="Your password">
-        </div>
-        <div class="column">
-          <button class="button is-primary">Login</button>
-          <button class="button is-secondary">Register</button>
-        </div>
+  <nav class="nav has-shadow">
+    <div class="container">
+      <div class="nav-item is-tab">
+        <input class="input" type="text" placeholder="Your username">
+      </div>
+      <div class="nav-item is-tab">
+        <input class="input" type="password" placeholder="Your password">
+      </div>
+      <div class="nav-item is-tab">
+        <button class="button is-primary">Login</button>
+      </div>
+      <div class="nav-item is-tab">
+        <router-link class="nav-item is-tab" to="/sign-up">
+          <button class="button is-secondary">Sign-Up</button>
+        </router-link>
       </div>
     </div>
-  </div>
+  </nav>
 </template>
